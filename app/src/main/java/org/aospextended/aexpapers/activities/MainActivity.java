d9437a355b0f92aec75ebaabb2a0c92f8be9be7d
ishubhamsingh <coolsks94@gmail.com>
2017-05-24 14:59:14 +0530

    Connect Firebase *Initial Analystics *Initial Notification

--- app/src/main/java/org/aospextended/aexpapers/activities/MainActivity.java
+++ app/src/main/java/org/aospextended/aexpapers/activities/MainActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import com.dm.wallpaper.board.activities.WallpaperBoardActivity;
 import org.aospextended.aexpapers.licenses.License;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends WallpaperBoardActivity {
 
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         initMainActivity(savedInstanceState,
                 License.isLicenseCheckerEnabled(),
                 License.getRandomString(),
