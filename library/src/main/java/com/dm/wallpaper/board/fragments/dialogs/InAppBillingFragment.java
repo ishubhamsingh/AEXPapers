839b221f5faa2c68953a115fbefa17fc944015d5
Shubham Singh <coolsks94@gmail.com>
2017-10-31 11:53:30 +0530

    Bringin wallpaperboard library as local library for AEXPapers *Commit history of the library can be found at https://github.com/danimahardhika/wallpaperboard

---
+++ library/src/main/java/com/dm/wallpaper/board/fragments/dialogs/InAppBillingFragment.java
@@ -0,0 +1,223 @@
+package com.dm.wallpaper.board.fragments.dialogs;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.SkuDetails;
+import com.dm.wallpaper.board.R;
+import com.dm.wallpaper.board.R2;
+import com.dm.wallpaper.board.adapters.InAppBillingAdapter;
+import com.dm.wallpaper.board.helpers.TypefaceHelper;
+import com.dm.wallpaper.board.items.InAppBilling;
+import com.dm.wallpaper.board.utils.Extras;
+import com.dm.wallpaper.board.utils.LogUtil;
+import com.dm.wallpaper.board.utils.listeners.InAppBillingListener;
+
+import java.lang.ref.WeakReference;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/*
+ * Wallpaper Board
+ *
+ * Copyright (c) 2017 Dani Mahardhika
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class InAppBillingFragment extends DialogFragment {
+
+    @BindView(R2.id.listview)
+    ListView mListView;
+    @BindView(R2.id.progress)
+    ProgressBar mProgress;
+
+    private String mKey;
+    private String[] mProductsId;
+
+    private InAppBillingAdapter mAdapter;
+    private AsyncTask<Void, Void, Boolean> mLoadInAppProducts;
+
+    private static WeakReference<BillingProcessor> mBillingProcessor;
+
+    private static final String TAG = "com.dm.wallpaper.board.dialog.inappbilling";
+
+    private static InAppBillingFragment newInstance(String key, String[] productId) {
+        InAppBillingFragment fragment = new InAppBillingFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(Extras.EXTRA_KEY, key);
+        bundle.putStringArray(Extras.EXTRA_PRODUCT_ID, productId);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    public static void showInAppBillingDialog(@NonNull FragmentManager fm, BillingProcessor billingProcessor,
+                                              @NonNull String key, @NonNull String[] productId) {
+        mBillingProcessor = new WeakReference<>(billingProcessor);
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag(TAG);
+        if (prev != null) {
+            ft.remove(prev);
+        }
+
+        try {
+            DialogFragment dialog = InAppBillingFragment.newInstance(key, productId);
+            dialog.show(ft, TAG);
+        } catch (IllegalArgumentException | IllegalStateException ignored) {}
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mKey = getArguments().getString(Extras.EXTRA_KEY);
+        mProductsId = getArguments().getStringArray(Extras.EXTRA_PRODUCT_ID);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity());
+        builder.customView(R.layout.fragment_inappbilling, false)
+                .typeface(TypefaceHelper.getMedium(getActivity()), TypefaceHelper.getRegular(getActivity()))
+                .title(R.string.navigation_view_donate)
+                .positiveText(R.string.donate)
+                .negativeText(R.string.close)
+                .onPositive((dialog, which) -> {
+                    if (mLoadInAppProducts == null) {
+                        try {
+                            InAppBillingListener listener = (InAppBillingListener) getActivity();
+                            listener.onInAppBillingSelected(mAdapter.getSelectedProduct());
+                        } catch (Exception ignored) {}
+                        dismiss();
+                    }
+                });
+
+        MaterialDialog dialog = builder.build();
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+        setCancelable(false);
+
+        ButterKnife.bind(this, dialog);
+        return dialog;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            mKey = savedInstanceState.getString(Extras.EXTRA_KEY);
+            mProductsId = savedInstanceState.getStringArray(Extras.EXTRA_PRODUCT_ID);
+        }
+        loadInAppProducts();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(Extras.EXTRA_KEY, mKey);
+        outState.putStringArray(Extras.EXTRA_PRODUCT_ID, mProductsId);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        mBillingProcessor = null;
+        if (mLoadInAppProducts != null) {
+            mLoadInAppProducts.cancel(true);
+        }
+        super.onDismiss(dialog);
+    }
+
+    private void loadInAppProducts() {
+        mLoadInAppProducts = new AsyncTask<Void, Void, Boolean>() {
+
+            InAppBilling[] inAppBillings;
+            boolean isBillingNotReady = false;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                mProgress.setVisibility(View.VISIBLE);
+                inAppBillings = new InAppBilling[mProductsId.length];
+            }
+
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                while (!isCancelled()) {
+                    try {
+                        Thread.sleep(1);
+                        if (mBillingProcessor == null) {
+                            isBillingNotReady = true;
+                            return false;
+                        }
+
+                        for (int i = 0; i < mProductsId.length; i++) {
+                            SkuDetails product = mBillingProcessor.get()
+                                    .getPurchaseListingDetails(mProductsId[i]);
+                            if (product != null) {
+                                InAppBilling inAppBilling;
+                                String title = product.title.substring(0, product.title.lastIndexOf("("));
+                                inAppBilling = new InAppBilling(product.priceText, mProductsId[i], title);
+                                inAppBillings[i] = inAppBilling;
+                            } else {
+                                if (i == mProductsId.length - 1)
+                                    return false;
+                            }
+                        }
+                        return true;
+                    } catch (Exception e) {
+                        LogUtil.e(Log.getStackTraceString(e));
+                        return false;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+                super.onPostExecute(aBoolean);
+                mLoadInAppProducts = null;
+
+                if (getActivity() == null) return;
+                if (getActivity().isFinishing()) return;
+
+                mProgress.setVisibility(View.GONE);
+                if (aBoolean) {
+                    mAdapter = new InAppBillingAdapter(getActivity(), inAppBillings);
+                    mListView.setAdapter(mAdapter);
+                } else {
+                    dismiss();
+                    if (!isBillingNotReady)
+                        Toast.makeText(getActivity(), R.string.billing_load_product_failed,
+                                Toast.LENGTH_LONG).show();
+                }
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+}
---
+++ library/src/main/java/com/dm/wallpaper/board/fragments/dialogs/LanguagesFragment.java
@@ -0,0 +1,156 @@
+package com.dm.wallpaper.board.fragments.dialogs;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.dm.wallpaper.board.R;
+import com.dm.wallpaper.board.R2;
+import com.dm.wallpaper.board.adapters.LanguagesAdapter;
+import com.dm.wallpaper.board.helpers.LocaleHelper;
+import com.dm.wallpaper.board.helpers.TypefaceHelper;
+import com.dm.wallpaper.board.items.Language;
+import com.dm.wallpaper.board.preferences.Preferences;
+import com.dm.wallpaper.board.utils.LogUtil;
+
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/*
+ * Wallpaper Board
+ *
+ * Copyright (c) 2017 Dani Mahardhika
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class LanguagesFragment extends DialogFragment {
+
+    @BindView(R2.id.listview)
+    ListView mListView;
+
+    private Locale mLocale;
+    private AsyncTask mAsyncTask;
+
+    public static final String TAG = "com.dm.wallpaper.board.dialog.languages";
+
+    private static LanguagesFragment newInstance() {
+        return new LanguagesFragment();
+    }
+
+    public static void showLanguageChooser(@NonNull FragmentManager fm) {
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag(TAG);
+        if (prev != null) {
+            ft.remove(prev);
+        }
+
+        try {
+            DialogFragment dialog = LanguagesFragment.newInstance();
+            dialog.show(ft, TAG);
+        } catch (IllegalArgumentException | IllegalStateException ignored) {}
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity());
+        builder.customView(R.layout.fragment_languages, false);
+        builder.typeface(TypefaceHelper.getMedium(getActivity()), TypefaceHelper.getRegular(getActivity()));
+        builder.title(R.string.pref_language_header);
+        MaterialDialog dialog = builder.build();
+        dialog.show();
+
+        ButterKnife.bind(this, dialog);
+        return dialog;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mAsyncTask = new LanguagesLoader().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        if (mLocale != null) {
+            Preferences.get(getActivity()).setCurrentLocale(mLocale.toString());
+            LocaleHelper.setLocale(getActivity());
+            getActivity().recreate();
+        }
+        if (mAsyncTask != null) {
+            mAsyncTask.cancel(true);
+        }
+        super.onDismiss(dialog);
+    }
+
+    public void setLanguage(@NonNull Locale locale) {
+        mLocale = locale;
+        dismiss();
+    }
+
+    private class LanguagesLoader extends AsyncTask<Void, Void, Boolean> {
+
+        private List<Language> languages;
+        private int index = 0;
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            while (!isCancelled()) {
+                try {
+                    Thread.sleep(1);
+                    languages = LocaleHelper.getAvailableLanguages(getActivity());
+                    Locale locale = Preferences.get(getActivity()).getCurrentLocale();
+                    for (int i = 0; i < languages.size(); i++) {
+                        Locale l = languages.get(i).getLocale();
+                        if (l.toString().equals(locale.toString())) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    return true;
+                } catch (Exception e) {
+                    LogUtil.e(Log.getStackTraceString(e));
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if (getActivity() == null) return;
+            if (getActivity().isFinishing()) return;
+
+            mAsyncTask = null;
+            if (aBoolean) {
+                mListView.setAdapter(new LanguagesAdapter(getActivity(), languages, index));
+            } else {
+                dismiss();
+            }
+        }
+    }
+}
